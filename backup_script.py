6598f0fa9564296388cef1ee17f696bfc9feb841
Bach Wumpus <frawgnuserver@gmail.com>
2023-05-07 22:53:09 +0700

    [skip ci] never give up

--- backup_script.py
+++ backup_script.py
@@ -1,62 +1,80 @@
 import os
-import datetime
 import csv
 import requests
+from datetime import date
+from pathlib import Path
 import git
 
-# Get Github token
-github_token = os.getenv('GITHUB_TOKEN')
 
-# Configure Github authentication
-session = requests.Session()
-session.auth = ('', github_token)
+def backup_articles(zendesk_url, language):
+    """
+    Download all the articles from a Zendesk help center in the specified language,
+    and store them as HTML files in a local directory named after today's date and the language.
+    """
+    # Get Github token
+    github_token = os.getenv('GITHUB_TOKEN')
 
-# Replace zendesk and language
-# e.g. zendesk = 'https://support.awesomepandas.com'
-# language = 'en-US'
+    # Configure Github authentication
+    session = requests.Session()
+    session.auth = ('', github_token)
 
-zendesk = 'https://support.discord.com/'
-language = 'en-US'
+    # Create the backup directory with the right name
+    backup_path = Path(date.today().strftime('%Y-%m-%d'), language.lower())
+    backup_path.mkdir(parents=True, exist_ok=True)
 
-date = datetime.date.today()
-backup_path = os.path.join(str(date), language)
-if not os.path.exists(backup_path):
-    os.makedirs(backup_path)
+    # Retrieve the articles from the help center
+    endpoint = f"{zendesk_url}/api/v2/help_center/{language}/articles.json"
+    while endpoint:
+        response = session.get(endpoint)
+        response.raise_for_status()
+        data = response.json()
 
-log = []
+        # Save each article to a file
+        for article in data['articles']:
+            try:
+                filename = f"{article['title']}.html"
+                filepath = backup_path / filename
 
-endpoint = zendesk + '/api/v2/help_center/{locale}/articles.json'.format(locale=language.lower())
-while endpoint:
-    response = session.get(endpoint)
-    if response.status_code != 200:
-        print('Failed to retrieve articles with error {}'.format(response.status_code))
-        exit()
-    data = response.json()
+                # Save the body of the article to a file
+                with filepath.open(mode='w', encoding='utf-8') as f:
+                    f.write(article['body'])
 
-    for article in data['articles']:
-        article_id = article['id']
-        filename = '{article_id}.html'.format(article_id=article_id)
-        filepath = os.path.join(backup_path, filename)
-        with open(filepath, mode='w', encoding='utf-8') as f:
-            f.write(article['body'])
-        print('{filename} copied!'.format(filename=filename))
+                print(f"Copied {filename}!")
+            except Exception as e:
+                print(f"Failed to save article {article['id']} with error: {e}")
 
-        log.append((filename, article['title'], article['author_id']))
+        endpoint = data.get('next_page')
 
-    endpoint = data['next_page']
 
-with open(os.path.join(backup_path, '_log.csv'), mode='wt', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(('File', 'Title', 'Author ID'))
-    for article in log:
-        writer.writerow(article)
+def commit_to_github(repo_path):
+    """
+    Commit any changes in the specified directory to a Github repository.
+    """
+    try:
+        # Open the repository
+        repo = git.Repo(repo_path)
 
-# Commit changes to repository
-try:
-    repo = git.Repo(os.getcwd())
-    repo.git.add('.')
-    repo.git.commit(m='Backup update for {date}'.format(date=date.strftime('%Y-%m-%d')))
-    repo.git.push()
-    print("Changes pushed to Github repository.")
-except Exception as e:
-    print(f"Failed to push changes to Github repository with error: {e}")
+        # Add all new and modified files
+        repo.git.add('--all')
+
+        # Commit the changes with a message
+        commit_message = f"Backup update for {date.today()}"
+        repo.git.commit(m=commit_message)
+
+        # Push the changes to Github
+        repo.git.push()
+
+        print("Changes pushed to Github repository.")
+    except Exception as e:
+        print(f"Failed to push changes to Github repository with error: {e}")
+
+
+
+if __name__ == "__main__":
+    # Set these variables with the URL of your Zendesk help center and the language code
+    zendesk_url = "https://support.discord.com"
+    language = "en-us"
+
+    # Backup the articles and commit to Github
+    backup_articles(zendesk_url, language)
+    commit_to_github(Path(__file__).parent)
